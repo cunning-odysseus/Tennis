760549873553753dda83e5c87e1a5b18f147757c
cunning-odysseus <caiovanbeek@outlook.com>
2024-06-07 23:26:05 +0200

    comments toegevoegd

--- leaderboard/app.py
+++ leaderboard/app.py
@@ -2,20 +2,32 @@ import os
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
+# Hier wordt een flask object gemaakt met de naam 'app'
 app = Flask(__name__)
 
-# Define the absolute path for the database directory
+# Hier wordt het huidige pad opgehaald
 base_dir = os.path.abspath(os.path.dirname(__file__))
+
+# Hier wordt de string van een nieuw pad gemaakt in de huidige directory
 db_dir = os.path.join(base_dir, 'data')
 
-# Ensure the 'data' directory exists
+# Hier wordt het pad gemaakt als het nog niet bestaat
 if not os.path.exists(db_dir):
     os.makedirs(db_dir)
 
-# Update the database URI to point to the 'data' directory with an absolute path
+# De app vertellen waar de database staat en wat voor database het is
+""" 
+a string used to configure the connection to a database. Its typically in the format of a URL and includes the 
+username, password, hostname, database name, and port number. The format of the URL is
+dialect+driver://username:password@host:port/database.
+"""
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(db_dir, "leaderboard.db")}'
+
+# Hier wordt de app verteld dat het niet aanpassingen moet loggen in een apart bestand
+# Dat zorgt namelijk voor overhead
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Maakt integratie tussen SQLAlchemy en de app
 db = SQLAlchemy(app)
 
 class Match(db.Model):
@@ -27,6 +39,8 @@ class Match(db.Model):
 with app.app_context():
     db.create_all()
 
+# De route decorator wordt gebruikt om Flask te vertellen welke URL de functie zou moeten triggeren
+# HTML content in de string wordt gerenderd
 @app.route('/')
 def index():
     leaderboard = Match.query.order_by(Match.score.desc()).all()
@@ -42,6 +56,6 @@ def add_match():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
 
 # test
